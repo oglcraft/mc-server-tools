af099f6edbe48cc91fc8663fd5270cde0c3400be
oglcraft <pritc014@gmail.com>
2019-01-27 14:13:40 -0600

    added snapshot parameter

--- check-latest/script.sh
+++ check-latest/script.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
+# parameters
+type=release
+
+if [ "$1" == "true" ] ; \
+  type=snapshot \
+; fi
+
 # LauncherMeta
 url='https://launchermeta.mojang.com/mc/game/version_manifest.json'
 
 # get the latest version from LauncherMeta
-echo $(curl -s $url | jq -r '.latest.snapshot')
+echo $(curl -s $url | jq -r ".latest.$type")
